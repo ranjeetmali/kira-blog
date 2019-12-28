924a89538998b12a59c1884dac681422fbbab1bc
Ranjeet <ranjeetmali49@gmail.com>
2019-12-28 17:33:41 +0530

    - first commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+
+    public function user()
+    {
+
+    }
+}
