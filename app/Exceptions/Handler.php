84813dfa2f693b940380897d455412e9cc8b85bc
Ranjeet <ranjeetmali49@gmail.com>
2020-02-17 11:12:03 +0530

    - bug fixed

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -16,7 +16,12 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        \Illuminate\Auth\AuthenticationException::class,
+        \Illuminate\Auth\Access\AuthorizationException::class,
+        \Symfony\Component\HttpKernel\Exception\HttpException::class,
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+        \Illuminate\Session\TokenMismatchException::class,
+        \Illuminate\Validation\ValidationException::class,
     ];
 
     /**
